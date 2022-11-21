35519e96ecb544b139cbd9c3d246468f30d0029b
mbakhtar <109742973+mbakhtar@users.noreply.github.com>
2022-11-21 11:06:53 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,9 @@
 let rotate = 0
 forever(function () {
     modules.led1.setPixelColor(rotate, 0xff0000)
-    pause(1000)
     modules.led1.rotate(1)
-    pause(1000)
     rotate += 1
-    pause(1000)
-    if (rotate == 8) {
+    if (rotate > 8) {
         rotate = 0
         modules.led1.setAll(0x000000)
     }
